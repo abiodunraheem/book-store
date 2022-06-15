2ee1b9219558e6c95f46be9aebdef0f0a226d58e
abiodunraheem <raheemabiodun23@gmail.com>
2022-06-15 01:30:40 +0100

    Add reduce in react component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <>
-    <div className="add-container">
-      <h2>ADD NEW BOOK</h2>
-      <form action="#" method="post">
-        <input type="text" placeholder="Book Title" />
-        <input type="text" placeholder="Book Author" />
-        <button type="button">Add Book</button>
-      </form>
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [infor, setInfor] = useState({ books: '', author: '' });
 
-    </div>
-  </>
-);
+  const inputData = (e) => {
+    const inputName = e.target.name;
+    switch (inputName) {
+      case 'title':
+        setInfor({ books: e.target.value, author: infor.author });
+        break;
+      case 'author':
+        setInfor({ books: infor.books, author: e.target.value });
+        break;
+      default:
+        setInfor({ books: infor.books, author: infor.author });
+    }
+  };
+
+  return (
+    <>
+      <div className="add-container">
+        <h2>ADD NEW BOOK</h2>
+        <form action="#" method="post">
+          <input type="text" name="title" placeholder="Book Title" onChange={inputData} />
+          <input type="text" name="author" placeholder="Book Author" onChange={inputData} />
+          <button type="button" onClick={() => dispatch(addBook(infor))}>Add Book</button>
+        </form>
+
+      </div>
+    </>
+  );
+};
 
 export default AddBook;
